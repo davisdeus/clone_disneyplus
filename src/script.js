2be5d7a67e0d13841f1137265b8c5e35af7550e0
Davi Silva de Deus <davisilvadedeus18@gmail.com>
2024-03-07 19:19:27 -0300

    ajustes no mobile

--- src/script.js
+++ src/script.js
@@ -1,7 +1,22 @@
 document.addEventListener('DOMContentLoaded', function () {
     const buttons = document.querySelectorAll('[data-tab-button]');
+    const questions = document.querySelectorAll('[data-faq-question]');
 
+    const heroSection = document.querySelector('.hero');
+    // pegando a altura do elemento
+    const alturaHero = heroSection.clientHeight;
 
+    window.addEventListener('scroll', function(){
+        const posicaoAtual = window.scrollY;
+
+        if (posicaoAtual < alturaHero){
+            ocultaElementosDoHeader();
+        } else {
+            exibirElementosDoHeader();
+        }
+    })
+
+    // Seção de alterações, programação das abas
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function (botao) {
             const abaAlvo = botao.target.dataset.tabButton;
@@ -13,7 +28,22 @@ document.addEventListener('DOMContentLoaded', function () {
 
         })
     }
+
+    // Seção FAQ acordion
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', abrirFechaResposta);
+    }
 })
+
+function ocultaElementosDoHeader() {
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden');
+}
+
+function exibirElementosDoHeader() {
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden');
+}
 
 function removeBotaoAtivo() {
     const buttons = document.querySelectorAll('[data-tab-button]');
@@ -32,4 +62,9 @@ function escondeTodasAsAbas() {
     }
 }
 
+function abrirFechaResposta (elemento){
+    const classe = 'faq__questions__item--is-open';
+    const elementoPai = elemento.target.parentNode;
 
+    elementoPai.classList.toggle(classe);
+}
